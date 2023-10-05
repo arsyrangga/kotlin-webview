3698f408fe16a1096e5ae97c31539e7565dc5c85
rangga <ranggantwleng77@gmail.com>
2023-10-05 14:13:11 +0700

    update

--- app/src/main/java/com/rangga/webview/MainActivity.kt
+++ app/src/main/java/com/rangga/webview/MainActivity.kt
@@ -3,12 +3,21 @@ package com.rangga.webview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val myWebView: WebView = findViewById(R.id.webview)
-        myWebView.loadUrl("https://www.dicoding.com")
+        myWebView.settings.javaScriptEnabled = true
+
+        myWebView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                Toast.makeText(this@MainActivity, "Web Dicoding berhasil dimuat", Toast.LENGTH_LONG).show()
+            }
+        }
+        myWebView.loadUrl("https://rangga.website")
     }
 }
