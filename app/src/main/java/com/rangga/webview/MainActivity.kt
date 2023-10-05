e106249a7c055d538e39900679f1f6e5a8dc977d
rangga <ranggantwleng77@gmail.com>
2023-10-05 14:10:19 +0700

    kotlin-webview

---
+++ app/src/main/java/com/rangga/webview/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.rangga.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl("https://www.dicoding.com")
+    }
+}
